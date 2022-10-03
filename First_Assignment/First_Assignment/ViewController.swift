8faaa9ba405f6e0301438c08b09a6f2ff2354668
CHO <93557885+loveitall@users.noreply.github.com>
2022-10-03 20:14:45 +0900

    init project

---
+++ First_Assignment/First_Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  First_Assignment
+//
+//  Created by 누리링 on 2022/10/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
